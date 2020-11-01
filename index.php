9ab44d6b7e0af3373873b302f2d8a0ee5734b7b9
czoller <czoller@users.noreply.github.com>
2020-11-02 00:30:09 +0100

    Converter für Means.TV, ARD und ZDF

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
+// TODO Werder.TV
+// TODO Stopp-Button einblenden, wenn Play erfolgreich
+
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$TESTMODE = true;
+$TESTMODE = false;
 
 $CONVERTERS = [];
+$CONVERTERS['meanstv'] = new class extends UrlConverter {
+    public static $REG_EX = '/^https?\:\/\/(www\.)?means\.tv\/.+$/';
 
+    public function convert($url) {
+        return "plugin://plugin.video.meanstv/?show=video&id=$url";
+    }
+};
 $CONVERTERS['youtube'] = new class extends UrlConverter {
     public static $REG_EX = '/^https?\:\/\/(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)(\w+)$/';
 
@@ -15,13 +24,35 @@ $CONVERTERS['youtube'] = new class extends UrlConverter {
         return preg_replace(self::$REG_EX, 'plugin://plugin.video.youtube/play/?video_id=\3', $url);
     }
 };
+$CONVERTERS['ard'] = new class extends UrlConverter {
+    public static $REG_EX = '/^https?\:\/\/(www\.)?ardmediathek\.de\/.+$/';
+
+    public function convert($url) {
+        $file = file_get_contents($url);
+        if (preg_match('/\"contentId\"\:(\d+)\,/', $file, $matches) == 1) {
+            $documentId = $matches[1];
+            return "plugin://plugin.video.ardmediathek_de/?mode=libArdPlay&documentId=$documentId";
+        }
+        return $url;
+    }
+};
+$CONVERTERS['zdf'] = new class extends UrlConverter {
+    public static $REG_EX = '/^https?\:\/\/(www\.)?zdf\.de((\/.+)\.html)$/';
+
+    public function convert($url) {
+        preg_match(self::$REG_EX, $url, $matches);
+        $contentName = urlencode("/zdf${matches[3]}");
+        $videoUrl = urlencode($matches[2]);
+        return "plugin://plugin.video.zdf_de_2016/?pagelet=PlayVideo&contentName=$contentName&videoUrl=$videoUrl";
+    }
+};
+
 
 if (!empty($_GET['file'])) {
     $file= trim($_GET['file']);
     foreach ($CONVERTERS as $converter) {
         $file = $converter->convertOrPass($file);        
     }
-    var_dump($file);
     play($file);
 }
 
@@ -56,8 +87,6 @@ abstract class UrlConverter {
     
     public function convertOrPass($url) {
         $class = get_class($this);
-        var_dump($class);
-        var_dump($class::$REG_EX);
         if (preg_match($class::$REG_EX, $url)) {
             return $this->convert($url);
         }
